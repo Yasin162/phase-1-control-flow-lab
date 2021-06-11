71e3fb6ca5091272ddb569002a30a9b2dfc1ea7f
Yasin <yasinabdussaburr@gmail.com>
2021-06-11 18:11:11 -0400

    commit tree 971ef1640e7724803cc87580695d068e65cd9e65

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(feet) {
   // Write your code here!
+  if (feet <= 400) {
+    return 'This one is on me!'
+  } else if (feet > 2000) {
+    if (feet > 2500) {
+      return 'No can do.'
+    } else {
+      return 'I will gladly take your thirty bucks.'
+    }
+  }
+};
+function ternaryCheckCity(city) {
+  return (city === 'NYC' ? "Ok, sounds good.": "No go.")
 }
-
-function ternaryCheckCity(){
   // Write your code here!
+function switchOnCharmFromTip(tip) {
+ // Write your code here!
+switch (tip) {
+case 'generous':
+  return 'Thank you so much.';
+break;
+case 'not as generous':
+  return 'Thank you.';
+break;
+case 'thanks for everything':
+  return 'Bye.';
+break;
 }
-
-function switchOnCharmFromTip(){
-  // Write your code here!
 }
